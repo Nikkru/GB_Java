7f4e098108406ebcbc5d85e7d6e1a9300c744b1d
NikNik <nickkru@gmail.com>
2022-12-15 20:56:23 +0300

    add homework 2 task 2

--- seminar_2/homework_2/homework_2.java
+++ seminar_2/homework_2/homework_2.java
@@ -1,7 +1,18 @@
 package seminar_2.homework_2;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class homework_2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /*
         * 1. Напишите метод, который принимает на вход строку (String) и
         * определяет является ли строка палиндромом (возвращает boolean значение).
@@ -12,6 +23,30 @@ public class homework_2 {
         String str2 = "abcdedcba";
         
         System.out.println(isPolindrom(str1));
+        /*
+         * 2. Напишите метод, который определит тип (расширение) файлов из текущей папки
+         * и выведет в консоль результат вида
+         * 1 Расширение файла: txt
+         * 2 Расширение файла: pdf
+         * 3 Расширение файла:
+         * 4 Расширение файла: jpg
+         */
+        Path file = Paths.get("");
+        List<Path> Directories = Files.list(file).collect(Collectors.toList());
+        List<String> str = new ArrayList<>();
+        System.out.println(Directories);
+        int directoriesSize = Directories.size();
+        for (int i = 0; i < directoriesSize; i++){
+            Path p = Directories.get(i);
+            str.add(p.toString());
+        }
+        for(String s : str){
+            int index = s.lastIndexOf(".");
+            if (index > 0){
+                String extension = s.substring(index + 1);
+                System.out.println("Расширение файла:" + extension);
+            }
+        }
     }
 
     private static String isPolindrom(String str) {
@@ -25,5 +60,18 @@ public class homework_2 {
             }
         }
         return isPolindrom;
+}
+    private static void getAllFiles(String path) {
+        File[] allFiles = new File(path).listFiles();
+        if (allFiles != null) {
+            for (File file : allFiles) {
+                String fileName = file.toString();
+                int index = fileName.lastIndexOf('.');
+                if(index > 0) {
+                    String extension = fileName.substring(index + 1);
+                    System.out.println("Расширение файла:" + extension);
+                }
+            }
+        }
 }
 }
