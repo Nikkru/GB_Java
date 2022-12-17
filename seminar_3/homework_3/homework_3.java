b9b7006c2d740051da29f9cb65074bad6800614c
NikNik <nickkru@gmail.com>
2022-12-17 17:15:13 +0300

    homework 3 add task 2

--- seminar_3/homework_3/homework_3.java
+++ seminar_3/homework_3/homework_3.java
@@ -2,6 +2,7 @@ package seminar_3.homework_3;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.ListIterator;
 
 public class homework_3 {
@@ -12,9 +13,9 @@ public class homework_3 {
          * (в языке уже есть что-то готовое для этого)
          */
         ArrayList<Integer> intList = new ArrayList<>(Arrays.asList(1, 4, 3, 56, 76, 89, 60, 54, 65, 77, 33, 45, 65, 89, 897));
-        ListIterator<Integer> iteratorList = intList.listIterator();
-        
+        ListIterator<Integer> iteratorList = intList.listIterator();       
         System.out.println(intList.toString());
+
         while (iteratorList.hasNext()) {
             Integer i = iteratorList.next();
             if(i%2==0) {
@@ -22,5 +23,21 @@ public class homework_3 {
             }
         }
         System.out.println(intList.toString());
+
+        /*
+         *  2. Задан целочисленный список ArrayList. 
+         * Найти минимальное, максимальное и среднее арифметическое из этого списка.
+        */
+        Collections.sort(intList);
+        Integer size = intList.size();
+
+    System.out.println("минимальное: " + intList.get(0));
+    System.out.println("максимальное: " + intList.get(size-1));
+
+    Integer sum_ = 0;
+    for (Integer i: intList){
+        sum_ += i;
+    }
+    System.out.println("среднее арифметическое: " + sum_/size);
     }
 }
